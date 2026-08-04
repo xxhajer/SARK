f07396b9df1f8b69353ef00b994881f279052f77
xxhajer <hajerthe911@gmail.com>
2026-08-04 10:50:54 +0300

    Initial Commit

---
+++ SARK/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SARK
+//
+//  Created by hajer almejel on 21/02/1448 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
