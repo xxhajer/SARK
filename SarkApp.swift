682bbd41a3519d58bd51493858acaa6ef1308aa8
itsdevruba <rubaaldawssari@gmail.com>
2026-08-10 09:42:44 +0300

    Add files via upload

---
+++ SarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SarkApp.swift
+//  Sark
+//
+//  Created by ربى خالد الدوسري on 23/02/1448 AH.
+//
+
+import SwiftUI
+
+@main
+struct SarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SplashView()
+        }
+    }
+}
